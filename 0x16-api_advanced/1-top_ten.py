9fb6df3789300738808502d4267a5f5d4190723e
Wongani <wonganichulu@gmail.com>
2024-08-14 15:08:33 +0200

    Fix-1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+"""Module that queries the reddit api"""
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """
+    script that queries the Reddit API and returns the top 10 hot post
+    to the subreddit
+    """
+    import requests
 
+    query_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                              .format(subreddit),
+                              headers={"User-Agent": "My-Agent"},
+                              allow_redirects=False)
+    if query_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in query_info.json().get("data").get("children")]
 # #!/usr/bin/python3
 # """
 # Defines function top_ten: Function that queries the Reddit API and
