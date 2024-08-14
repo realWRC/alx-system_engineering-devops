1600903847bb28ff8c256790f76e2f6654ff4641
Wongani <wonganichulu@gmail.com>
2024-08-14 15:53:53 +0200

    Added puppet fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
-# Puppet script to replace and correct syntax errors.
-
 # Correct all intances of phpp syntax error to php
-
-$file = '/var/www/html/wp-settings.php'
-
 exec { 'correct_syntax':
-  command => "sed -i 's/phpp/php/g' ${file}",
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/bin', '/usr/bin'],
   onlyif  => "grep 'phpp' ${file}",
 }
