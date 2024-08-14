216e324d185126284cfa3d3e69ad3f07d45552ca
Wongani <wonganichulu@gmail.com>
2024-08-14 09:36:12 +0200

    Added 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# Puppet script to replace and correct syntax errors.
+
+# Correct all intances of phpp syntax error to php
+
+$file = '/var/www/html/wp-settings.php'
+
+exec { 'correct_syntax':
+  command => "sed -i 's/phpp/php/g' ${file}",
+  path    => ['bin', '/usr/bin']
+}
