2e8434c6d28dd3bdd40c4c97ed21a8720085ae7d
Wongani <wonganichulu@gmail.com>
2024-08-14 15:20:29 +0200

    fix-4

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,28 +13,69 @@ def top_ten(subreddit):
     Function that queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit.
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    browser = {'User-Agent': 'Google Chrome Version 127.0.6533.100'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {
-            "limit": 10
-            }
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"limit": 10}
     response = requests.get(
-            url, headers=browser, params=params, allow_redirects=False
+            url, headers=user_agent, params=params, allow_redirects=False
             )
-
-    if response.status_code == 404:
-        print("None")
-        return
-
-    result = response.json()
     try:
-        for i in range(0, 10):
-            post = result.get('data').get('children')[i].get('data')\
-                    .get('title')
-            print(post)
+        response.raise_for_status()
+        result = response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print("HTTP error occurred: {}".format(http_err))
+        return None
+    except requests.exceptions.RequestException as req_err:
+        print("Error occurred: {}".format(req_err))
+        return None
+    except ValueError:
+        print("Response content is not valid JSON or is empty.")
+        return None
 
-    except Exception:
-        print("None")
+    if result.get("data"):
+        for child in result.get("data").get("children", []):
+            print(child.get("data").get("title"))
+    else:
+        print("No data found in the response.")
+        return None
+
+# #!/usr/bin/python3
+# """
+# Defines function top_ten: Function that queries the Reddit API and
+# prints the titles of the first 10 hot posts listed for a
+# given subreddit.
+# """
+#
+# import requests
+#
+#
+# def top_ten(subreddit):
+#     """
+#     Function that queries the Reddit API and prints the titles of the
+#     first 10 hot posts listed for a given subreddit.
+#     """
+#     if subreddit is None or not isinstance(subreddit, str):
+#         print("None")
+#
+#     browser = {'User-Agent': 'Google Chrome Version 127.0.6533.100'}
+#     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+#     params = {
+#             "limit": 10
+#             }
+#     response = requests.get(
+#             url, headers=browser, params=params, allow_redirects=False
+#             )
+#
+#     if response.status_code == 404:
+#         print("None")
+#         return
+#
+#     result = response.json()
+#     try:
+#         for i in range(0, 10):
+#             post = result.get('data').get('children')[i].get('data')\
+#                     .get('title')
+#             print(post)
+#
+#     except Exception:
+#         print("None")
