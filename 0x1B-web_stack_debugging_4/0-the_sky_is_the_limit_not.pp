382a02f94ea3f191a3f2d2637d3cdf4011a2dfc2
Wongani <wonganichulu@gmail.com>
2024-08-20 05:27:46 +0200

    Added 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increses the ULIMIT to 4096
+exec { 'increase_ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  notify  => Exec['nginx-restart'],
+}
+
+# Restarts nginx
+exec { 'nginx-restart':
+  command       => '/etc/init.d/nginx restart',
+  path          => ['/usr/sbin', '/sbin', '/usr/local/bin', '/bin', '/usr/bin'],
+  refreshonly   => true,
+}
